7c2c8c5a36c3095b03cb4f155b7bde5a75a25ce9
Cynth <cyntheticneutral@gmail.com>
2022-04-03 12:21:18 -0700

    v 3

--- userscript.user.js
+++ userscript.user.js
@@ -3,7 +3,7 @@
 // @namespace   http://tampermonkey.net/
 // @match       https://hot-potato.reddit.com/embed*
 // @grant       none
-// @version     0.2
+// @version     0.3
 // @author      cynthe#3157
 // @description Protecc the necc
 // @icon        https://i1266.photobucket.com/albums/jj534/KAIZA2002/NACIcon.png
@@ -15,7 +15,7 @@ if (window.top !== window.self) {
         (function () {
             const i = document.createElement("img");
             i.src = "https://raw.githubusercontent.com/cynthetic-boop/neco-arc-overlay/main/place-neco-arc-overlay.png";
-            i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 1000px;";
+            i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 2000px;";
             console.log(i);
             return i;
         })())
